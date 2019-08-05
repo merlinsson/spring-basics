a1762eedfab37a04df059a66c20db6beaa39aa5d
mrothhardt <merlin.rothhardt@direkt-gruppe.de>
2019-08-05 13:54:56 +0200

    Reformatted and changed bean def

--- src/main/java/de/workshop/spring/chapter1/HelloWorld.java
+++ src/main/java/de/workshop/spring/chapter1/HelloWorld.java
@@ -4,10 +4,11 @@ package de.workshop.spring.chapter1;
 public class HelloWorld {
     private String message;
 
-    public void setMessage(String message){
-        this.message  = message;
+    public void setMessage(String message) {
+        this.message = message;
     }
-    public void getMessage(){
+
+    public void getMessage() {
         System.out.println("Your Message : " + message);
     }
 }
